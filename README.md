69725994469d861c9fce3fd481f59f164a27d746
yuxiaotao <yuxiaotao@unicloud.com>
2019-10-16 14:01:57 +0800

    yxtc-li

--- README.md
+++ README.md
@@ -1,2 +1,4 @@
-# yxt-cli
-脚手架
+// 全局安装脚手架
+npm install -g y-cli
+// 拉取预定义模板
+y-cli init
---
+++ bin/index.js
@@ -0,0 +1,33 @@
+#!/usr/bin/env node
+'use strict'
+// 引入 commander，用于处理自定义nodejs命令
+const program = require('commander')
+// 引用package.json里面的版本号来定义当前版本
+program
+    .version(require('../package').version )
+// 定义使用方法
+program
+    .usage('<command>')
+
+// 定义init命令，同时定义init命令的简化命令 i，包括命令的脚本文件所在路径
+program
+    .command('init')
+    .description('pull a new project')
+    .alias('i')
+    .option('-t, --type [type]', 'project type')
+    .action(() => {
+        require('../command/init')()
+    })
+
+// 这一句必不可少，作用是解析命令行参数argv，这里的process.argv是nodejs全局对象的属性
+program.parse(process.argv)
+
+// console.log(process.argv)
+console.log(program.commands[0].type)
+console.log('======')
+
+// 处理参数和提供帮助信息
+if(!program.args.length){
+    program.help()
+}
+
