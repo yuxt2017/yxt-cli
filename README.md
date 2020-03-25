2a2ef7fe4c69ba9a904e1ebddb83cc4ecce140c9
yuxiaotao <yuxiaotao@unicloud.com>
2020-03-25 17:10:31 +0800

    修改库

--- README.md
+++ README.md
@@ -1,4 +1,11 @@
-// 全局安装脚手架
-npm install -g y-cli
-// 拉取预定义模板
-y-cli init
+### 全局安装脚手架
+npm install -g yxt-cli
+### 拉取预定义模板
+yxt-cli init [option]
+### eg：yxt-cli init --pc | --h5
+
+### option
+#### --pc 拉取pc通用模板
+#### --h5 拉取H5通用模板
+
+#### 后续待更新
--- command/init.js
+++ command/init.js
@@ -1,23 +1,70 @@
-'use strict'
-const exec = require('child_process').exec
-const projectUrl = 'https://github.com/yuxt2017/y-template.git'
+const clone = require('git-clone')
+const fs= require('fs')
+const shell = require('shelljs');
+const log = require('tracer').colorConsole()
+const inquirer = require('inquirer')
+/*
+*inquirer功能简介
+*		input–输入
+*		validate–验证
+*		list–列表选项
+*		confirm–提示
+*		checkbox–复选框等等
+*/
 
-module.exports = () => {
-    console.log('start command')
+module.exports = (program) => {
+    program
+        .command('init [project]')
+        .description('get a template')
+        .alias('i')
+        .option('-p --pc', 'pc')
+        .option('-h --h5', 'h5')
+        .action((project, cmd) => {
+            log.info('pc or h5; example：yxt-cli init myproject --pc | --h5')
 
-    // git命令，远程拉取项目并自定义项目名
-    let cmdStr = `git clone `+projectUrl
+            var promps = [];
+            if (project == undefined || project == null) {
+                promps.push({
+                    type: 'input',
+                    name: 'project',
+                    message: 'please enter a name of your project',
+                    validate: function (input) {
+                        if (input) {
+                            return true
+                        } else {
+                            return 'project name cannot be empty'
+                        }
+                    }
+                })
+            }
 
-    // console.log(process)
-
-    // 在nodejs中执行shell命令，第一个参数是命令，第二个是具体的回调函数
-    exec(cmdStr, (error, stdout, stderr) => {
-        if (error) {
-            console.log(error)
-            process.exit()
-        }
-        console.log('pull success')
-        process.exit()
-    })
-
+            inquirer.prompt(promps).then((answers) => {
+                let projectName = project || answers.project
+                let pwd = shell.pwd()
+                let url;
+                if(cmd.pc){
+                    url = `https://github.com/yuxt2017/web-front.git`;
+                }else{
+                    url = `https://github.com/yuxt2017/h5-front.git`;
+                }
+                log.info(`start pull ${url}...`)
+                fs.exists(`${projectName}`, (fileExists) => {
+                    if(!fileExists) {
+                        clone(url, pwd + `/${projectName}`, null, function() {
+                            shell.rm('-rf', pwd + `/${projectName}/.git`)
+                            shell.cd(`${projectName}`);
+                            log.info(`npm install...`)
+                            if (shell.exec('npm i').code !== 0) {
+                                shell.echo('Error: npm i failed'); // 输出内容
+                                shell.exit(1); // 退出
+                            } else {
+                                log.info('done success')
+                            }
+                        })
+                    } else {
+                        log.error('The folder already exists')
+                    }
+                });
+            })
+        })
 }
